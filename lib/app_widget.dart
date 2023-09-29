937a2a0de3c9c69366dab3d2fecec7251cc4c761
Alexandre Souza <alexsz1609@gmail.com>
2023-09-29 16:38:27 -0300

    organização do app e estilização

---
+++ lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+
+  const AppWidget({super.key,});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
