a9d947f2fda0ee63cf3daeee3abb0c63db63550d
Alexandre Souza <alexsz1609@gmail.com>
2023-09-30 17:46:28 -0300

    adicionando a função de incremento

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,14 +14,19 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.red,
-        title: const Text('Home Page'),
+        title: const Text(
+          '-- ++',
+        ),
       ),
       body: Center(
         child: GestureDetector(
           child: Text(
-            'Contador: $counter',
-            style: const TextStyle(fontSize: 25,),
+            'Contador $counter',
+            style: const TextStyle(
+              fontSize: 30,
+            ),
           ),
           onTap: () {
             setState(() {
@@ -30,14 +35,39 @@ class HomePageState extends State<HomePage> {
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        child: const Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom:
+                16.0, // Ajuste essa posição verticalmente conforme necessário
+            left:
+                55.0, // Ajuste essa posição horizontalmente conforme necessário
+            child: FloatingActionButton.large(
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.remove),
+              onPressed: () {
+                setState(() {
+                  counter--;
+                });
+              },
+            ),
+          ),
+          Positioned(
+            bottom:
+                16.0, // Ajuste essa posição verticalmente conforme necessário
+            right:
+                25.0, // Ajuste essa posição horizontalmente conforme necessário
+            child: FloatingActionButton.large(
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  counter++;
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
